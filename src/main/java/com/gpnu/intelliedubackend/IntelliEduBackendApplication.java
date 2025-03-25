f063e4207833b8bbc480f35b3ecab50f1170b2c7
TakahashiRika <2511399469@qq.com>
2025-03-25 22:39:59 +0800

    Initial commit

---
+++ src/main/java/com/gpnu/intelliedubackend/IntelliEduBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gpnu.intelliedubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliEduBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliEduBackendApplication.class, args);
+    }
+
+}
